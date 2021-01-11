78d5a07a32c76c3d6fac0f0578eadde72cd1751b
Luis Felipe Klaus <duckbox@hotmail.com>
2021-01-11 19:45:44 -0300

    card-grid

--- src/App.js
+++ src/App.js
@@ -1,58 +1,8 @@
 import React, { useState } from "react";
-import { makeStyles, useTheme,    ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import { Button, Grid, Paper, Switch, Typography } from "@material-ui/core";
-
-
-const useStyles = makeStyles(theme => ({/*
-  root: {
-    height: "100vh",
-    background: "linear-gradient(45deg, #FE6888 30%, #FF8553 90%)"
-  },
-  button: {
-    backgroundColor: "blue",
-    [theme.breakpoints.up('sm')]: {backgroundColor: "black",}
-  }
-*/}));
 
 export default function App() {
-  // dark/light mode toggler
-  const [darkMode, setDarkMode] = useState(false)
-
-  const theme = createMuiTheme({
-    palette: {
-      type: darkMode ? "dark" : "light",
-    },
-  });
-
-
-
-
-  const classes = useStyles();
-  //const theme = useTheme();
-  console.log(theme.palette.type);
-
+  
   return (
-    <ThemeProvider theme={theme}>
-      <Paper 
-        style={{ height: "100vh"}}
-        elevation={0}
-        //square
-        className={classes.root}>
-        <Grid container direction="column">
-          <Typography variant="h2"> 
-            This is my App!
-          </Typography>
-          <Button className={classes.button} color="primary" variant="contained">
-            This is a button 
-          </Button>
-          <Button className={classes.button} color="secondary" variant="contained">
-            This is another button 
-          </Button>
-          
-          <Switch color="secondary" checked={darkMode} onChange={() => setDarkMode(!darkMode)}>
-          </Switch>
-        </Grid>
-      </Paper>
-    </ThemeProvider>
+    <h1>HELLO</h1>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 
 
-import mytheme from './mytheme';
 
 const rootElement = document.getElementById('root')
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={mytheme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>,
   rootElement
 );
