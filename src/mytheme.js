fbf3b3d2119901df1e1f665be2e0e3758584f1fb
Luis Felipe Klaus <duckbox@hotmail.com>
2021-01-10 21:56:33 -0300

    themeProvider, createMuiTHeme, useTheme

---
+++ src/mytheme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+//import { green, purple} from '@material-ui/core/colors';
+
+const mytheme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+});
+
+export default mytheme
